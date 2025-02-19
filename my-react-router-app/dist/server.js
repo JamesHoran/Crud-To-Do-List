b70a39c99aab6c5941f2d2cd4037bfcc0bca9feb
James Horan <jbolton128@gmail.com>
2025-02-19 17:14:20 -0600

    No longer working react app with working react button and react server

---
+++ my-react-router-app/dist/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+const port = 3001;
+const app = express();
+app.use(cors());
+// app.use(express.json());
+// app.get('/', (req: Request, res: Response) => {
+app.get(`http://localhost/${port}`, (req, res) => {
+    res.send({ message: "json text here that has been updated" });
+});
+// app.get('/more-data', (req: Request, res: Response) => {
+//   res.send({ message: "less data is here"})
+// })
+app.listen(port, () => {
+    console.log(`Server Started Successfully`);
+});
