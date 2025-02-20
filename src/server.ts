271f285f38eb870045b098dc15743f84f975201e
James Horan <jbolton128@gmail.com>
2025-02-20 10:16:03 -0600

    built server

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import type { Request, Response, Express } from 'express';
+import express from 'express';
+import cors from 'cors';
+
+const port = 3001
+const app: Express = express();
+
+app.use(cors());
+// app.use(express.json());
+
+// app.get('/', (req: Request, res: Response) => {
+app.get(`http://localhost/${port}`, (req: Request, res: Response) => {
+  res.send({ message: "json text here that has been updated"})
+})
+
+app.get('/more-data', (req: Request, res: Response) => {
+  res.send({ message: "less data is here"})
+})
+
+app.listen(port, () => {
+  console.log(`Server Started Successfully`)
+})
